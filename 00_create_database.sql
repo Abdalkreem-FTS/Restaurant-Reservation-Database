0e46495aee16470582c1e775d021a6b0481fab96
Abdalkreem Bzoor <abdalkreem.bzoor@foothillsolutions.com>
2026-07-17 11:42:39 +0300

    Add create database script

--- 00_create_database.sql
+++ 00_create_database.sql
@@ -1,15 +1,3 @@
-USE master;
-GO
 
-IF DB_ID('RestaurantReservationDB') IS NOT NULL
-BEGIN
-    ALTER DATABASE RestaurantReservationDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE RestaurantReservationDB;
-END
-GO
-
-CREATE DATABASE RestaurantReservationDB;
-GO
-
-USE RestaurantReservationDB;
-GO
+DROP DATABASE IF EXISTS restaurantreservationdb WITH (FORCE);
+CREATE DATABASE restaurantreservationdb;
