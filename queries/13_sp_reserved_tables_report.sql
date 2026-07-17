d2aeee72274d18ed7d4e14a4ac17acb85c516aa5
Abdalkreem Bzoor <abdalkreem.bzoor@foothillsolutions.com>
2026-07-17 10:33:05 +0300

    Req 13: Add sp_ReservedTablesReport procedure

---
+++ queries/13_sp_reserved_tables_report.sql
@@ -0,0 +1,20 @@
+-- Requirement 13: Report of tables reserved within a date range, with reservation date, party size and restaurant details.
+
+CREATE OR ALTER PROCEDURE dbo.sp_ReservedTablesReport
+    @StartDate DATETIME,
+    @EndDate DATETIME
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT t.TableId, rt.RestaurantId, rt.[Name] AS RestaurantName, res.ReservationDate,
+           res.PartySize, t.Capacity, rt.[Address], rt.PhoneNumber, rt.OpeningHours
+    FROM Reservations AS res
+    JOIN [Tables] AS t ON res.TableId = t.TableId
+    JOIN Restaurants AS rt ON res.RestaurantId = rt.RestaurantId
+    WHERE res.ReservationDate BETWEEN @StartDate AND @EndDate
+    ORDER BY res.ReservationDate, rt.RestaurantId, t.TableId;
+END
+GO
+
+EXEC dbo.sp_ReservedTablesReport @StartDate = '2026-01-01', @EndDate = '2026-03-31';
