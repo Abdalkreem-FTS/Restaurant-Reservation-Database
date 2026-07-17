20f17c4c5b8b404071030bee64014dd0898f2ca0
Abdalkreem Bzoor <abdalkreem.bzoor@foothillsolutions.com>
2026-07-17 10:32:58 +0300

    Req 7: Add EmployeesDetails view

---
+++ queries/07_view_employee_details.sql
@@ -0,0 +1,20 @@
+-- Requirement 7: A view listing every employee together with their restaurant details.
+
+CREATE OR ALTER VIEW EmployeesDetails AS
+SELECT e.EmployeeId,
+       e.FirstName,
+       e.LastName,
+       CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
+       e.Position,
+       r.RestaurantId,
+       r.[Name] AS RestaurantName,
+       r.[Address] AS RestaurantAddress,
+       r.PhoneNumber AS RestaurantPhone,
+       r.OpeningHours
+FROM Employees AS e
+JOIN Restaurants AS r ON e.RestaurantId = r.RestaurantId;
+GO
+
+SELECT *
+FROM EmployeesDetails
+ORDER BY RestaurantId, EmployeeId;
